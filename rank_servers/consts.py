f20ee130ab4045ed4909ffe4d388e92810b8c3e9
Ren Tatsumoto <tatsu@autistici.org>
2021-12-31 09:14:35 +0300

    tweak script

--- rank_servers/consts.py
+++ rank_servers/consts.py
@@ -12,10 +12,8 @@ __all__ = [
 ]
 
 
-def data_dir() -> str:
+def find_data_dir() -> str:
     if d := os.getenv('MATRIX_DATA_DIR'):
-        return d
-    elif os.path.isdir(d := os.path.join(os.pardir, 'res')):
         return d
     else:
         raise FileNotFoundError("Data dir not found.")
@@ -27,7 +25,7 @@ def blocklist_escape(s: str) -> str:
 
 def construct_blocklist() -> list[str]:
     blocklist = []
-    with open(os.path.join(__d, 'blocklist.json')) as f:
+    with open(os.path.join(data_dir, 'blocklist.json')) as f:
         for server in json.load(f):
             blocklist.append(blocklist_escape(server['name']))
             if server['name'].startswith('*.'):
@@ -36,13 +34,13 @@ def construct_blocklist() -> list[str]:
     return blocklist
 
 
-__d = data_dir()
+data_dir = find_data_dir()
 
 URL = 'https://the-federation.info/graphql?query=query%20Platform(%24name%3A%20String)%20%7B%0A%20%20platforms(name%3A%20%24name)%20%7B%0A%20%20%20%20name%0A%20%20%20%20code%0A%20%20%20%20displayName%0A%20%20%20%20description%0A%20%20%20%20tagline%0A%20%20%20%20website%0A%20%20%20%20icon%0A%20%20%20%20__typename%0A%20%20%7D%0A%20%20nodes(platform%3A%20%24name)%20%7B%0A%20%20%20%20id%0A%20%20%20%20name%0A%20%20%20%20version%0A%20%20%20%20openSignups%0A%20%20%20%20host%0A%20%20%20%20platform%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20icon%0A%20%20%20%20%20%20__typename%0A%20%20%20%20%7D%0A%20%20%20%20countryCode%0A%20%20%20%20countryFlag%0A%20%20%20%20countryName%0A%20%20%20%20services%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20__typename%0A%20%20%20%20%7D%0A%20%20%20%20__typename%0A%20%20%7D%0A%20%20statsGlobalToday(platform%3A%20%24name)%20%7B%0A%20%20%20%20usersTotal%0A%20%20%20%20usersHalfYear%0A%20%20%20%20usersMonthly%0A%20%20%20%20localPosts%0A%20%20%20%20localComments%0A%20%20%20%20__typename%0A%20%20%7D%0A%20%20statsNodes(platform%3A%20%24name)%20%7B%0A%20%20%20%20node%20%7B%0A%20%20%20%20%20%20id%0A%20%20%20%20%20%20__typename%0A%20%20%20%20%7D%0A%20%20%20%20usersTotal%0A%20%20%20%20usersHalfYear%0A%20%20%20%20usersMonthly%0A%20%20%20%20localPosts%0A%20%20%20%20localComments%0A%20%20%20%20__typename%0A%20%20%7D%0A%7D%0A&operationName=Platform&variables=%7B%22name%22%3A%22matrix%7Csynapse%22%7D'
-JSON_FILEPATH = os.path.join(__d, 'servers.json')
-RESULT_FILEPATH = os.path.join(__d, 'result.tsv')
-FORMATTED_FILEPATH = os.path.join(__d, 'formatted.html')
-TEMPLATE_FILEPATH = os.path.join(__d, 'template.html')
+JSON_FILEPATH = os.path.join(data_dir, 'servers.json')
+RESULT_FILEPATH = os.path.join(data_dir, 'result.tsv')
+FORMATTED_FILEPATH = os.path.join(data_dir, 'formatted.html')
+TEMPLATE_FILEPATH = os.path.join(data_dir, 'template.html')
 BLOCKLIST = construct_blocklist()
 
 try:
--- run.sh
+++ run.sh
@@ -16,12 +16,13 @@ latest_synapse() {
 mkdir -p -- "$MATRIX_DATA_DIR"
 cd -- "$dir" || exit 1
 
+echo "Latest synapse version: $LATEST_SYNAPSE"
+
 export MATRIX_DATA_DIR LATEST_SYNAPSE
-{
-	[[ -z $* ]] || rm -- "$MATRIX_DATA_DIR/servers.json" || true
-	python3 -m rank_servers
-	mv -- "$MATRIX_DATA_DIR/formatted.html" "$dir/index.html"
-	mv -- "$MATRIX_DATA_DIR/result.tsv" "$dir/result.tsv"
-} 2>/dev/null
+
+rm -- "$MATRIX_DATA_DIR/servers.json" || true
+python3 -m rank_servers
+mv -- "$MATRIX_DATA_DIR/formatted.html" "$dir/index.html"
+mv -- "$MATRIX_DATA_DIR/result.tsv" "$dir/result.tsv"
 
 echo "Done."
