b5ce5d22f7eb085ddb2a96e05c771ddf2b8fd922
Ren Tatsumoto <tatsu@autistici.org>
2023-12-27 19:24:05 +0300

    fix api url

--- download/sources/joinmatrix.py
+++ download/sources/joinmatrix.py
@@ -4,19 +4,19 @@ from ..server_info import ServerInfo, rm_port, version_trim
 
 
 def joinmatrix_url():
-    return 'https://joinmatrix.org/servers.json'
+    return 'https://servers.joinmatrix.org/servers.json'
 
 
 async def download_joinmatrix(client: httpx.AsyncClient) -> list[ServerInfo]:
     data: httpx.Response = await client.get(joinmatrix_url())
     data: list[dict] = data.json()
     out = []
-    for server in data:
+    for server in data['public_servers']:
         out.append(ServerInfo(
             name=rm_port(server['name']),
-            url=rm_port(server['domain']),
-            country=server['jurisdiction'],
-            open_signups=server['open'],
+            url=rm_port(server['client_domain']),
+            country=server['staff_jur'],
+            open_signups=True,
             software=server['software'],
             version=version_trim(server['version']),
             sourced_from="Join Matrix",
